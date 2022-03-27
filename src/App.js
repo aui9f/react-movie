79f41ebcb8bbe52171dad348635420e67618a3d0
sjpark <aui9f@naver.com>
2022-03-27 21:47:14 +0900

    react study (todo)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import {useState} from 'react';
 
+function App(){
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-function App() {
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    console.log("!!")
+    event.preventDefault();
+    if(todo===''){
+      return;
+    }
+    
+    setTodos(currentArray=>[todo, ...currentArray]);
+    setTodo('');
+    
+  }
+  console.log(todos)
+
   return (
     <div>
-      <h1>HELLO</h1>
-    </div>
-  );
-}
+      <h3>My To Dos ({todos.length})</h3>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="Write your to do"
+          onChange={onChange}
+          value={todo}
+        />
+        <button type="button" onClick={onSubmit}>Add Todo</button>
+      </form>
+      <hr/>
+      <ul>
+      {todos.map((x, index) => ( <li key={index}>{x}</li> ))}
+      </ul>
 
+      
+    </div>
+  )
+}
 export default App;
